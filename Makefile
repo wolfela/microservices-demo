89fc7780db1117d07bfd2520a1392488ab68cbc1
Tatiana Kmita <eteralny@gmail.com>
2018-05-10 19:15:14 +0100

    Cleaned up server file and created Makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 build:
+	go build server.go
+
+	go build client.go
+
 	protoc -I. --go_out=plugins=grpc:$(GOPATH)roomone/microservices-demo/ \
 	  protoc/numerical.proto
--- server.go
+++ server.go
@@ -4,11 +4,9 @@ import (
 	"log"
 	"net"
 
-	// Import the generated protobuf code
 	pb "./protoc"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -19,7 +17,6 @@ type service struct {
 }
 
 func (s *service) SumArray(ctx context.Context, req *pb.Array) (*pb.Response, error) {
-
 	var sum int32
 	sum = 0
 
@@ -27,8 +24,6 @@ func (s *service) SumArray(ctx context.Context, req *pb.Array) (*pb.Response, er
 		sum += req.Ints[i]
 	}
 
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
 	return &pb.Response{Result: sum}, nil
 }
 
@@ -42,21 +37,14 @@ func (s *service) SumWords(ctx context.Context, req *pb.StringArray) (*pb.String
 }
 
 func main() {
-
-	// Set-up our gRPC server.
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
 
-	// Register our service with the gRPC server, this will tie our
-	// implementation into the auto-generated interface code for our
-	// protobuf definition.
+	s := grpc.NewServer()
 	pb.RegisterNumericalServiceServer(s, &service{})
 
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
